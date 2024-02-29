5959fc24abd4fdc3ca0e0f275afc1d2701b78538
junah201~ <junah.dev@gmail.com>
2024-02-29 23:26:47 +0900

    ✨ send_test_notification lambda 추가

---
+++ lambdas/send_test_notification/main.py
@@ -0,0 +1,143 @@
+"""
+테스트 알림을 보내는 람다 함수입니다.
+/notifications/test POST
+"""
+
+import json
+from datetime import datetime
+
+try:
+    from common import *
+except ImportError:
+    # for local test
+    from layers.common.python.common import *
+
+import boto3
+
+dynamodb = boto3.client('dynamodb')
+
+
+def middleware(event, context):
+    body = json.loads(event.get("body", "{}"))
+
+    chzzk_id = body.get("chzzk_id", None)
+    discord_channel_id = body.get("channel_id", None)
+
+    res = dynamodb.query(
+        TableName='chzzk-bot-db',
+        KeyConditionExpression='PK = :pk_val AND begins_with(SK, :sk_val)',
+        ExpressionAttributeValues={
+            ':pk_val': {'S': f'CHZZK#{discord_channel_id}'},
+            ':sk_val': {'S': f'NOTI#{chzzk_id}'}
+        }
+    )
+
+    if res["Count"] < 1:
+        return {
+            "statusCode": 404,
+            "headers": {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            "body": json.dumps({"message": "해당 알림이 존재하지 않습니다."}),
+        }
+
+    item = res["Items"][0]
+
+    res = send_message(
+        channel_id=discord_channel_id,
+        data={
+            "content": f"관리자의 요청에 따라 전송된 테스트 알림입니다." + "\n\n" + item.get("custom_message", {}).get("S", ""),
+            "embeds": [
+                {
+                    "title": f"{chzzk.liveTitle}",
+                    "description": f"{chzzk.channel.channelName} 님이 방송을 시작했습니다.",
+                    "color": 0x02E895,
+                    "fields": [
+                        {
+                            "name": '카테고리',
+                            "value": chzzk.liveCategoryValue
+                        }
+                    ],
+                    "image": {
+                        "url": (chzzk.liveImageUrl or chzzk.channel.channelImageUrl or "").replace("_{type}", "_1080"),
+                    },
+                    "author": {
+                        "name": f"{chzzk.channel.channelName}",
+                        "url": f"https://chzzk.naver.com/live/{chzzk_id}",
+                        "icon_url": chzzk.channel.channelImageUrl or "https://ssl.pstatic.net/cmstatic/nng/img/img_anonymous_square_gray_opacity2x.png?type=f120_120_na"
+                    },
+                    "footer": {
+                        "text": "치직"
+                    },
+                    "url": f"https://chzzk.naver.com/live/{chzzk_id}",
+                    "timestamp": datetime.now().isoformat()
+                },
+            ],
+            "components": [
+                {
+                    "type": COMPONENT_TYPE.ACTION_ROW,
+                    "components": [
+                        {
+                            "type": COMPONENT_TYPE.BUTTON,
+                            "label": "바로가기",
+                            "style": BUTTON_STYLE.LINK,
+                            "url": f"https://chzzk.naver.com/live/{chzzk_id}"
+                        }
+                    ]
+                },
+            ],
+        }
+    )
+
+    # 채널이 존재하지 않는 경우
+    if res.status_code == 404:
+        print("channel not found")
+        dynamodb.delete_item(
+            TableName='chzzk-bot-db',
+            Key={
+                'PK': item.get('PK'),
+                'SK': item.get('SK')
+            }
+        )
+        return {
+            "statusCode": 404,
+            "headers": {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            "body": json.dumps({"message": "해당 디스코드 채널이 존재하지 않습니다."}),
+        }
+
+    # 메시지 전송에 실패한 경우
+    if res.status_code != 200:
+        print("send message fail", res.status_code)
+        print(res.json())
+        return {
+            "statusCode": 500,
+            "headers": {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            "body": json.dumps({"message": f"테스트 알림 전송에 실패했습니다. ({res.json()})"}),
+        }
+
+    # 성공
+    print("send message success")
+    return {
+        "statusCode": 200,
+        "headers": {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        "body": json.dumps({"message": "테스트 알림을 성공적으로 전송했습니다."}),
+    }
+
+
+def lambda_handler(event, context):
+    res = middleware(event, context)
+
+    print("=== res ===")
+    print(json.dumps(res))
+
+    return res
