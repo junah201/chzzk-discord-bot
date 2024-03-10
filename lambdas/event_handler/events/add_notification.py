9d44d099f0eff4a26655e82222ac7b8afbe5f8af
junah201~ <junah.dev@gmail.com>
2024-03-11 00:32:17 +0900

    🐛샤딩 개수 수정

--- lambdas/event_handler/events/add_notification.py
+++ lambdas/event_handler/events/add_notification.py
@@ -107,7 +107,7 @@ def handler(event, context):
                 'channelName': {'S': chzzk.channel.channelName},
                 'channelImageUrl': {'S': chzzk.channel.channelImageUrl or ""},
                 "type": {"S": "CHZZK"},
-                "index": {"N": f"{int(Chzzk_id, 16) % 10}"}
+                "index": {"N": f"{int(Chzzk_id, 16) % 6}"}
             }
         )
 
