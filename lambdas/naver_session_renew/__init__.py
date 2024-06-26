a7b154bd8e36cd7a6a0431664479dc3c548befe9
junah201 <junah.dev@gmail.com>
2024-06-26 20:38:34 +0900

    ✨ 세션 갱신 시 expired 에러 수정

--- lambdas/naver_session_renew/__init__.py
+++ lambdas/naver_session_renew/__init__.py
@@ -29,6 +29,7 @@ def handler(event, context):
         dynamo_to_python(item)
         for item in res.get('Items', [])
     ]
+    items.sort(key=lambda x: x.get("PK"))
 
     result = list()
 
@@ -43,26 +44,42 @@ def handler(event, context):
                 "Cookie": f"NID_AUT={NID_AUT}; NID_SES={NID_SES}"
             },
         )
-        NEW_NID_SES = res.headers["set-cookie"].split("NID_SES=")[
-            1].split(";")[0]
+        try:
+            NEW_NID_SES = res.headers["set-cookie"].split("NID_SES=")[
+                1].split(";")[0]
+        except Exception as e:
+            logger.error(
+                json.dumps(
+                    {
+                        "type": "NAVER_SESSION_RENEW_ERROR",
+                        "PK": item.get("PK"),
+                        "error": str(e),
+                        "text": res.text
+                    }
+                )
+            )
+            NEW_NID_SES = "error"
 
-        dynamodb.update_item(
-            TableName='chzzk-bot-db',
-            Key={
-                'PK': {'S': item.get("PK")},
-                'SK': {'S': item.get("SK")}
-            },
-            UpdateExpression='SET NID_SES = :val',
-            ExpressionAttributeValues={
-                ':val': {'S': NEW_NID_SES}
-            }
-        )
+        if NEW_NID_SES not in ["expired", "error"]:
+            dynamodb.update_item(
+                TableName='chzzk-bot-db',
+                Key={
+                    'PK': {'S': item.get("PK")},
+                    'SK': {'S': item.get("SK")}
+                },
+                UpdateExpression='SET NID_SES = :val',
+                ExpressionAttributeValues={
+                    ':val': {'S': NEW_NID_SES}
+                }
+            )
+
         result.append({
             "PK": item.get("PK"),
+            "NID_AUT": NID_AUT,
             "ORIGINAL_NID_SES": f"{NID_SES[:7]}...{NID_SES[-7:]}",
-            "NEW_NID_SES": f"{NEW_NID_SES[:7]}...{NEW_NID_SES[-7:]}"
+            "NEW_NID_SES": f"{NEW_NID_SES[:7]}...{NEW_NID_SES[-7:]}" if NEW_NID_SES not in ["expired", "error"] else NEW_NID_SES
         })
-        time.sleep(0.5)
+        time.sleep(2)
 
     logger.info(
         json.dumps(
