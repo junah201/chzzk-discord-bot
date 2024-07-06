6e01c2e95a7edc24c52350f71f5625a119ce5dd2
junah201 <junah.dev@gmail.com>
2024-07-06 22:19:47 +0900

    🚑️ 슬래시 명령어 지원 종료

--- lambdas/event_handler/__init__.py
+++ lambdas/event_handler/__init__.py
@@ -74,6 +74,27 @@ def handler(event, context):
             )
         }
 
+    # 슬래시 명령어 지원 종료 (from 2024.07.06)
+    res = {
+        "type": INTERACTION_CALLBACK_TYPE.CHANNEL_MESSAGE_WITH_SOURCE,
+        "data": {
+            "embeds": [
+                {
+                    "title": "슬래시 명령어 지원 종료 안내",
+                    "description": "`**2024.07.06**부터 치직 봇은 슬래시 명령어를 지원하지 않습니다. 이제부터는 [치직 봇 웹사이트](https://chzzk.junah.dev)를 사용해주세요. 또한 [치직 봇 공식 서버](https://api.chzzk.junah.dev/support-server)에서 도움을 받을 수 있습니다.",
+                    "color": 0x02E895,
+                    "footer": {
+                        "text": "치직 chzzk.junah.dev"
+                    }
+                }
+            ],
+        }
+    }
+    return {
+        "statusCode": 200,
+        "body": json.dumps(res, ensure_ascii=False)
+    }
+
     # event handler
     command_name = body.get("data", {}).get("name", None)
     logging.info(
