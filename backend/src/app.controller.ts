802bbec002ea7b3aeddaf6a7c16100c80d400084
junah201 <junah.dev@gmail.com>
2025-07-19 22:58:24 +0900

    ✨ Add Root Route

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -7,7 +7,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  root(): string {
+    return this.appService.root();
   }
 }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  root(): string {
+    const nowUtc = new Date().toISOString();
+    return `Notification API (UTC: ${nowUtc})`;
   }
 }
