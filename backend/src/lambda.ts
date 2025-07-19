4442fee7eb4b7dad7eff83ef769270b2f99bdce0
junah201 <junah.dev@gmail.com>
2025-07-19 11:27:01 +0900

    🎉 Setup initial NestJS backend boilerplate

---
+++ backend/src/lambda.ts
@@ -0,0 +1,33 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import serverlessExpress from '@vendia/serverless-express';
+import { APIGatewayProxyHandler, Handler } from 'aws-lambda';
+import express from 'express';
+
+import { AppModule } from './app.module';
+
+let cachedServer: Handler;
+
+const bootstrapServer = async (): Promise<Handler> => {
+  const expressApp = express();
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressApp),
+  );
+  app.enableCors();
+  await app.init();
+  return serverlessExpress({
+    app: expressApp,
+  });
+};
+
+export const handler: APIGatewayProxyHandler = async (
+  event,
+  context,
+  callback,
+) => {
+  if (!cachedServer) {
+    cachedServer = await bootstrapServer();
+  }
+  return cachedServer(event, context, callback);
+};
